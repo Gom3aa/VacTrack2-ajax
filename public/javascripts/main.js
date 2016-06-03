7c372e330907a665a2411725445cf1a183313d99
Gom3aa <m.gomaa_ca@hotmail.com>
2016-06-03 16:44:43 -0400

    commit message

---
+++ public/javascripts/main.js
@@ -0,0 +1,23 @@
+// $(function(){
+//  $('#search').on('keyup', function(e){
+//    if(e.keyCode === 13) {
+//      console.log('the key is 13');
+//      var parameters = { search: $(this).val() };
+//        $.get( 'searching',parameters, function(data) {
+//        $('#results').html(data);
+//      });
+//     };
+//  });
+// });
+
+
+function f1(){
+  alert('IN-f1');
+  console.log('IN-f1');
+  // var parameters = { search: document.getElementById('search').value};
+  // .get( '/searchinge',parameters, function(data) {
+  // //$('#results').html(data);
+  // document.getElementById('results').innerHTML = '100';
+  // });
+  alert('ENd-f1');
+};
